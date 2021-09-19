143cb03466c198a1c0388af9af6e63eeebedb9f5
ManuTheCoder <77016441+ManuTheCoder@users.noreply.github.com>
2021-09-19 18:10:09 +0000

    Code obfuscation is soooo cool, but it is soooo slow at the same time :(

--- index.js
+++ index.js
@@ -13,192 +13,24 @@ var customFilter = new Filter({ placeHolder: '*'});
 var CleanCSS = require('clean-css');
 var minify = require('html-minifier').minify;
 var JavaScriptObfuscator = require('javascript-obfuscator');
-var dd = ""
+var dd = "";
+var cache = {};
+var joinCache = {};
+var cors = require('cors');
 
-var cors = require('cors')
-app.use(cors())
+function _0xfdad(_0x593cb3,_0x20c6a2){var _0xfdad50=_0x20c6();return _0xfdad=function(_0x44ee78,_0x883683){_0x44ee78=_0x44ee78-0x177;var _0x54a425=_0xfdad50[_0x44ee78];return _0x54a425;},_0xfdad(_0x593cb3,_0x20c6a2);}function includeFile(_0x471ee1){var _0xf0943c=_0xfdad;eval(fs[_0xf0943c(0x177)](_0x471ee1,_0xf0943c(0x178)));}function _0x20c6(){var _0x291615=['readFileSync','utf-8'];_0x20c6=function(){return _0x291615;};return _0x20c6();}
 
-app.get("/", (req, res, next) => next());
 
-var _logger = (req, res, next) => {
-  next();
-  var url = __dirname + "/public" + req.url || "";
-	if(url !== "" && req.headers.host == "joinmypoll.ml") {
-			var urll = __dirname + "/public/" + req.url;
-			res.setHeader("Content-Type",  mime.lookup(path.extname(urll)));
-			console.log(urll)
-			if(urll == __dirname + "/public/" + "/") {
-				urll = __dirname + "/public/join.html"
-			}
-			var cc = fs.readFileSync(urll).toString();
-			cc = minify(cc, {
-			removeAttributeQuotes: true,
-			minifyJS: true,
-			collapseWhitespace: true,
-			continueOnParseError: true,
-			minifyCSS: true,
-		});
-			cc = cc.split("${__vars/hostname}").join(req.headers.host)
-			res.write(cc);
-			res.end();
-			return false;
-	}
-	else if(url.includes("/api")) {
-		var d = JSON.parse(fs.readFileSync("./public/database/polls.json").toString());
-		if(d[req.query.id]) {
-			res.write("true")
-		}
-		else {
-			res.write("false")
-		}
-		res.end();
-		return false;
-	}
-  if (url.endsWith("/") || !url.includes(".")) {
-    url += "/";
-    url = url + "index.html";
-  }
-  console.log(url);
-	var isPoll = false;
-  if (url.includes("/v/") && url !== "/home/runner/beta/public/v/style.css" && url !== "/home/runner/beta/public/v/app.js") {
-		isPoll = true;
-    var $_GET = url.split("/v/");
-    req.query.id = $_GET[1];
-    req.query.embed = "true";
-    url = $_GET[0] + "/v/index.html";
-    url = url;
-  }
-	if (url.includes("/e/") && url !== "/home/runner/beta/public/v/style.css" && url !== "/home/runner/beta/public/v/app.js") {
-		isPoll = true;
-    var $_GET = url.split("/e/");
-    req.query.id = $_GET[1];
-    url = $_GET[0] + "/v/index.html";
-    url = url;
-  }
-  if (url.includes("/r/")) {
-		isPoll = true;
-    var $_GET = url.split("/r/");
-    req.query.results = $_GET[1];
-    url = $_GET[0] + "/v/index.html";
-    url = url;
-  }
-	if (url.includes("/d/")) {
-		isPoll = true;
-    var $_GET = url.split("/d/");
-    req.query.results = $_GET[1];
-    url = $_GET[0] + "/delete.html";
-    url = url;
-  }
-	if (url.includes("/j/")) {
-    var $_GET = url.split("/j/");
-    req.query.id = $_GET[1];
-    url = $_GET[0] + "/add/index.html";
-    url = url;
-  }
-	if(fs.existsSync(url) === false) {
-		res.redirect("https://"+req.headers.host+"/v/404");
-		return false;
-	}
-  res.setHeader("Content-Type", mime.lookup(path.extname(url)));
-	var content = fs.readFileSync(url, { encoding: "utf-8" }).toString();
-	if(isPoll == true) {
-		var dbPolls = JSON.parse(fs.readFileSync(__dirname + "/public/database/polls.json"));
-		var id = $_GET[1].replace(/\D/g,'');
-		if(dbPolls[id]) {
-			content = content.split("${__vars/title}").join( dbPolls[id].title);
-			content = content.split("${__vars/description}").join(dbPolls[id].desc);
-			if(dbPolls[id].image) {
-				content = content.split("${__vars/banner}").join(dbPolls[id].image.toString().replace("?w=500", "") || "https://i.ibb.co/4jQj7tF/s.png");
-			}
-			else {
-					content = content.split("${__vars/banner}").join("https://i.ibb.co/4jQj7tF/s.png");
-			}
-			content = content.split("${__vars/id}").join(id);
-		}
-	}
-	if(path.extname(url) == ".css") {
-		var input = content;
-		var options = {};
-		content = new CleanCSS(options).minify(input).styles
-	}
-	else if(path.extname(url) == ".html") {
-		content = content.replace("{{DBTOKEN}}", process.env.__dbToken)
-		var result = minify(content, {
-			removeAttributeQuotes: true,
-			minifyJS: true,
-			collapseWhitespace: true,
-			continueOnParseError: true,
-			minifyCSS: true,
-		});
-		content = result.replace(/(\r\n|\n|\r)/gm, "").trim();
-		content = result.split("${__vars/hostname}").join(req.headers.host)
-	}
-	else if(path.extname(url) == ".js") {
-		
-		
-		content = content.split("${__vars/hostname}").join(req.headers.host)
-		content = content.replace("{{DBTOKEN}}", process.env.__dbToken)
-		if(dd == "" && url.includes("app.js")) {
-		content = JavaScriptObfuscator.obfuscate(content, {
-        compact: false,
-        controlFlowFlattening: true,
-        controlFlowFlatteningThreshold: 1,
-        numbersToExpressions: true,
-        simplify: false,
-        shuffleStringArray: true,
-        splitStrings: true,
-        stringArrayThreshold: 1,
-    }).getObfuscatedCode();
-		var result = minify(`<script id="DEL_TAG_SCRIPT">
-		${content}
-		</script>`, {
-			minifyJS: true,
-		});
-		result = result.replace(`<script id="DEL_TAG_SCRIPT">`, "")
-		result = result.replace(`</script>`, "")
-		content = result;
-		dd = content;
-		}
-		else {
-			if(url.includes("app.js")) {
-				content = dd;
-			}
-			else {
-				content = JavaScriptObfuscator.obfuscate(content, {
-        compact: false,
-        controlFlowFlattening: true,
-        controlFlowFlatteningThreshold: 1,
-        numbersToExpressions: true,
-        simplify: false,
-        shuffleStringArray: true,
-        splitStrings: true,
-        stringArrayThreshold: 1,
-    }).getObfuscatedCode();
-		var result = minify(`<script id="DEL_TAG_SCRIPT">
-		${content}
-		</script>`, {
-			minifyJS: true,
-		});
-		result = result.replace(`<script id="DEL_TAG_SCRIPT">`, "")
-		result = result.replace(`</script>`, "")
-		content = result;
-			}
-		}
-		
-		
-	}
-	else if(path.extname(url) == ".json" && req.header("Authorization") !== "Bearer "+ process.env.__dbToken) {
-		res.redirect("https://http.cat/403");
-		res.end()
-	}
-  res.send(content);
-  res.end();
-};
-app.use(_logger);
-httpserver.listen(3000);
+includeFile("./include/server.js");
 
 io.on("connection", (socket) => {
   socket.on("addPoll", (name, options, categories, desc, image, pwd) => {
+		console.log("New Poll Added!");
+		// if(socket) {}
+		// else {
+		// 	console.log("Error!")
+		// 	return false
+		// }
     var db = JSON.parse(fs.readFileSync("./public/database/polls.json"));
     var categories1 = [], options1 = [];
 		if(categories !== "") {
@@ -206,7 +38,7 @@ io.on("connection", (socket) => {
 		}
     options.split("\n").forEach(o => {
 			if(o.trim() !== "") {
-			options1.push({name:customFilter.clean(o),votes:0})
+			options1.push({name:customFilter.clean(o),votes:0});
 			}
 		});
     var i =
@@ -228,7 +60,7 @@ io.on("connection", (socket) => {
     io.emit("newPollAdded", i);
   });
   socket.on("votedNow", (a, b) => io.emit("votedNow", a, b));
-	socket.on("confetti", (id) => io.emit("confetti", id));
+  socket.on("confetti", (id) => io.emit("confetti", id));
 
   socket.on("vote", (optionID, pollID) => {
     io.emit("vote", optionID);
@@ -242,4 +74,3 @@ io.on("connection", (socket) => {
     io.emit("votedNow", pollID, db[pollID].options);
   });
 });
-
